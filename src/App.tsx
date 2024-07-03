f63d92d5919167e2cbde019deb0e3fb29db822b1
Benton Bergstrom <bbergstrom0524@gmail.com>
2024-07-03 11:33:39 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import WebpageBundle from "./WebpageBundle"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+      <div>
+        <WebpageBundle/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/WebpageBundle.tsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function WebpageBundle(){
+
+    return(
+        <>
+        <Header />
+        Hello!
+        </>
+    )
+}
+
+
+export default WebpageBundle;
